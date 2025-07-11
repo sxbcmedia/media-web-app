d00b6d3681112249e8527c95a8478f83c812919d
sxbcmedia <sxbcmedia@gmail.com>
2025-07-11 13:34:04 +0530

    upgraded packages

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <main>
-      <h1 className="text-5xl text-orange-500 font-bold">Welcome to the App</h1>
+      <h1 className="text-5xl text-orange-500 font-bold font-circular-web">Welcome to the App</h1>
     </main>
   );
 };
