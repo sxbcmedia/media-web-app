bba126ff46e7e39c5aa99554accbda7fbe1ec0a6
Sxbc Media <sxbcmedia@gmail.com>
2025-07-14 08:28:44 +0530

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-        fontfamily: {
-            zentry: ['zentry', 'sans-serif'],
-            general : ['general', 'sans-serif'],
-            'circular-web': ['circular-web', 'sans-serif'],
-            'robert-medium': ['robert-medium', 'sans-serif'],
-            'robert-regular': ['robert-regular', 'sans-serif'],
-        }
+      fontfamily: {
+        zentry: ["zentry", "sans-serif"],
+        general: ["general", "sans-serif"],
+        "circular-web": ["circular-web", "sans-serif"],
+        "robert-medium": ["robert-medium", "sans-serif"],
+        "robert-regular": ["robert-regular", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
+};
