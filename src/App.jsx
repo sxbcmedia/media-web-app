a5c3d028f103e63f2fc14d1c677fa2846338d232
Sxbc Media <sxbcmedia@gmail.com>
2025-07-15 07:28:19 +0530

    Hero section file creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <main>
-      <h1 className="text-5xl text-orange-500 font-bold font-circular-web text-[#FF6A3D]">Welcome to the App</h1>
+      <Hero />
     </main>
   );
 };
