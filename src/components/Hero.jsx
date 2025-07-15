9b0893b52fe273b8368350abecddcdda4b8238f5
Sxbc Media <sxbcmedia@gmail.com>
2025-07-15 14:50:43 +0530

    index.css made different with v4 tailwind font declaration

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,9 @@ const Hero = () => {
         className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
       >
         <div>
-          <div className="mask-clip-path"></div>
+          <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+            <div>MiniVideoPlayer</div>
+          </div>
         </div>
       </div>
     </div>
