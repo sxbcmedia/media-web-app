7d41aee4f5a6c908fdaedfbcf9fdc992a425d5d0
Sxbc Media <sxbcmedia@gmail.com>
2025-07-16 14:09:46 +0530

    video player component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,27 @@
-import {useRef, useState} from "react";
+import { useRef, useState } from "react";
 
 const Hero = () => {
-    const [currentIndex, setCurrentIndex] = useState(1);
-    const [hasCicked, setHasCicked] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);
-    const [loadedVideos, setLoadedVideos] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [hasCicked, setHasCicked] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedVideos, setLoadedVideos] = useState(0);
 
-    const totalVideos = 4;
-    const nextVideoRef = useRef(null);
+  const totalVideos = 4;
+  const nextVideoRef = useRef(null);
 
-    const handleVideoLoad = () =>{
-        setLoadedVideos((prev) => prev + 1);
-    }
-    
-    const handleMiniVdClick = () =>{
-        setHasCicked(true);
+  const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
 
-        setCurrentIndex((prevIndex) => prevIndex +1);
-    }
+  const handleVideoLoad = () => {
+    setLoadedVideos((prev) => prev + 1);
+  };
 
-    const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
+  const handleMiniVdClick = () => {
+    setHasCicked(true);
+
+    setCurrentIndex(upcomingVideoIndex);
+  };
+
+  const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
@@ -29,18 +31,38 @@ const Hero = () => {
       >
         <div>
           <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
-            <div onClick={handleMiniVdClick} className="origin-center">
-                <video
-                    ref={nextVideoRef}
-                    src={getVideoSrc(currentIndex + 1)}
-                    loop
-                    muted
-                    id="current-video"
-                    className="size-64 origin-center scale-150 object-cover object-center"
-                    onLoadedData={handleVideoLoad}
-                />
+            <div
+              onClick={handleMiniVdClick}
+              className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
+            >
+              <video
+                ref={nextVideoRef}
+                src={getVideoSrc(upcomingVideoIndex)}
+                loop
+                muted
+                id="current-video"
+                className="size-64 origin-center scale-150 object-cover object-center"
+                onLoadedData={handleVideoLoad}
+              />
             </div>
           </div>
+
+          <video
+            ref={nextVideoRef}
+            src={getVideoSrc(currentIndex)}
+            loop
+            muted
+            id="next-video"
+            className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+            onLoadedData={handleVideoLoad}
+          />
+
+          <video
+            src={getVideoSrc(
+              currentIndex === totalVideos - 1 ? 1 : currentIndex
+            )}
+            autoPlay
+          />
         </div>
       </div>
     </div>
