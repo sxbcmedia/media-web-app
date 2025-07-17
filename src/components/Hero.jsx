34fa75245fcd096dceb2d090a22b9a8c08f93789
sxbcmedia <sxbcmedia@gmail.com>
2025-07-17 14:53:08 +0530

    Finished Hero Section with GSAP animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,8 @@
 import { useRef, useState } from "react";
 import Button from "./Button";
 import { TiLocationArrow } from "react-icons/ti";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -22,6 +24,49 @@ const Hero = () => {
 
     setCurrentIndex(upcomingVideoIndex);
   };
+
+  useGSAP(()=>{
+    if(hasCicked){
+      gsap.set('#next-video', {visibility: 'visible'});
+
+      gsap.to('#next-video', {
+        transformOrigin: 'center center',
+        scale: 1,
+        width: '100%',
+        height: '100%',
+        duration: 1,
+        ease: 'power1.inOut',
+        onStart: () => nextVideoRef.current.play(),
+      })
+
+      gsap.from('#current-video', {
+        transformOrigin: 'center center',
+        scale: 0,
+        duration: 1.5,
+        ease: 'power1.inOut',
+
+      })
+    }
+  }, {dependencies:[currentIndex], revertOnUpdate: true})
+
+  useGSAP(()=>{
+    gsap.set('#video-frame', {
+      clipPath: 'polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)',
+      borderRadius: '0 0 40% 10%'
+    })
+
+    gsap.from('#video-frame', {
+      clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+      borderRadius: '0 0 0 0',
+      ease: 'power1.inOut',
+      scrollTrigger:{
+        trigger: '#video-frame',
+        start: 'center center',
+        end: 'bottom center', 
+        scrub: true,
+      }
+    })
+  })
 
   const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
@@ -63,7 +108,7 @@ const Hero = () => {
             src={getVideoSrc(
               currentIndex === totalVideos - 1 ? 1 : currentIndex
             )}
-            // autoPlay
+            autoPlay
             loop
             muted
             className="absolute left-0 top-0 size-full object-cover object-center"
@@ -90,11 +135,15 @@ const Hero = () => {
               id="watch-trailer"
               title="Watch Trailer"
               leftIcon={<TiLocationArrow />}
-              containerClass="bg-yellow-300 flex-center gap-1"
+              containerClass="!bg-yellow-300 flex-center gap-1"
             />
           </div>
         </div>
       </div>
+
+       <h1 className=" special-font hero-heading absolute bottom-5 right-5 text-black tracking-wide">
+          G<b>a</b>MING
+        </h1>
     </div>
   );
 };
