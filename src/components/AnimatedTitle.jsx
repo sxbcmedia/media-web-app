4599a46a12c1c10cc41f0e5b81d1f46842c7b59f
Sxbc Media <sxbcmedia@gmail.com>
2025-07-21 11:05:08 +0530

    small change

--- src/components/AnimatedTitle.jsx
+++ src/components/AnimatedTitle.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const AnimatedTitle = ({title, containerClass}) => {
+const AnimatedTitle = ({ title, containerClass }) => {
   return (
-    <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
-        {title.split('<b />').map((line, index) =>{
-            <div key={index} className="flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3">
-                {line.split(' ').map((word, i)=>(
-                    <span key={i} className="animated-word"></span>
-                ))}
-            </div>
-        })}
+    <div className={`animated-title ${containerClass}`}>
+      {title.split("<b />").map((line, index) => {
+        <div
+          key={index}
+          className="flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3"
+        >
+          {line.split(" ").map((word, i) => (
+            <span
+              key={i}
+              className="animated-word"
+              dangerouslySetInnerHTML={{ __html: word }}
+            />
+          ))}
+        </div>;
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default AnimatedTitle
+export default AnimatedTitle;
