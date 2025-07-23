f5353ab9a6dbb574142706ae0c3b53b74440a746
Sxbc Media <sxbcmedia@gmail.com>
2025-07-23 14:33:57 +0530

    added navBar audio Element

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from "./components/NavBar";
 
 const App = () => {
   return (
-    <main className=" relative min-h-screen w-screen overflow-x-hidden bg-zinc-600">
+    <main className=" relative min-h-screen w-screen overflow-x-hidden">
       <Navbar />
       <Hero />
       <About />
